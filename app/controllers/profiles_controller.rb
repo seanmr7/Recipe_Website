c78654d2f2a67793b7963f968047f65d4530077b
Sean <smriedl@gmail.com>
2021-01-11 18:42:48 -0500

    Create profiles and users controllers. Define new and create methods for ProfilesController

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,23 @@
+class ProfilesController < ApplicationController
+
+  def new
+    @profile = Profile.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @profile = @user.build_profile( profile_params )
+    if @profile.save
+      flash[:success] = "Profile updated!"
+      redirect_to user_path(params[:user_id])
+    else
+      render action: :new
+    end
+  end
+
+
+  private
+    def profile_params
+      params.require(:profile).permit(:first_name, :last_name, :username)
+    end
+end
