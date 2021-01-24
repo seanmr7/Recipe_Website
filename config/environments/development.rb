9f7a9b2e737631c290708eb09638e5c93f14112f
Sean Riedl <smriedl@gmail.com>
2021-01-23 19:52:18 -0500

    Fix user profile image

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,9 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  # Store profile images on local file system
+  config.active_storage.service = :local
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
