dfe5f71f1c35024d9f3dbf713d3b50a060ed9f99
Sean Riedl <smriedl@gmail.com>
2021-01-30 14:16:13 -0500

    Add links to user tag view on tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -4,10 +4,12 @@ class TagsController < ApplicationController
     
   end
 
+  # GET request to user_related_recipes. Requires user id, tag id, and recipe id.
+  # Lists recipes with tag id
   def user_related_recipes
     @tag = Tag.find(params[:id])
     @recipe = Recipe.find(params[:recipe_id])
-    @recipes = Recipe.joins(:taggings).where('recipes.id != ?', @recipe.id).where(taggings: { tag_id: @tag.id })
+    @recipes = Recipe.joins(:taggings).where('recipes.id != ? OR recipes.id = ?', @recipe.id, @recipe.id).where(taggings: { tag_id: @tag.id })
   end
 
   def new
