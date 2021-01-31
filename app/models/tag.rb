b4fe2b6269838631dcdc17c92a1b2c1b154648f3
Sean Riedl <smriedl@gmail.com>
2021-01-30 19:33:40 -0500

    Position alert messages

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,6 @@
 class Tag < ApplicationRecord
-  has_many :tag_maps
-  has_many :recipes, through: :tag_maps
+  has_many :taggings
+  has_many :recipes, through: :taggings
 
   def to_s
     tag_name
